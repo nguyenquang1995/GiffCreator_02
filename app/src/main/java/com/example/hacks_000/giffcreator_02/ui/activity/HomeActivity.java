9c9dff3e739420a562664b003910978d581bb96d
framgia.nguyen.van.quang <framgia@framgia0147-lt.framgia.com>
2016-06-09 14:06:08 +0700

    get image from facebook

--- app/src/main/java/com/example/hacks_000/giffcreator_02/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/hacks_000/giffcreator_02/ui/activity/HomeActivity.java
@@ -19,12 +19,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.hacks_000.giffcreator_02.R;
+import com.example.hacks_000.giffcreator_02.data.model.FacebookConstant;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements FacebookConstant {
     private static final int MY_PERMISSIONS_READ_EXTERNAL_STORAGE = 1;
     private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 2;
     private static final int MY_PERMISSIONS_USE_CAMERA = 3;
@@ -38,11 +56,17 @@ public class HomeActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private FloatingActionButton mFab;
     private Uri mCaptureImageUri;
+    private CallbackManager mCallbackManager;
+    private AccessToken mToken;
+    private List mImageLink = new ArrayList();
+    private int mAlbumGot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         findView();
+        setUptoUseFacebook();
     }
 
     private void findView() {
@@ -58,6 +82,76 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    private void setUptoUseFacebook() {
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        AppEventsLogger.activateApp(this.getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mToken = loginResult.getAccessToken();
+                String userId = mToken.getUserId();
+                GraphRequest.Callback getAlbumIdCallback = new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse graphResponse) {
+                        JSONObject jsonObject = graphResponse.getJSONObject();
+                        try {
+                            JSONArray array = jsonObject.getJSONArray(FACEBOOK_DATA);
+                            int lengthAlbum = array.length();
+                            for (int i = 0; i < lengthAlbum; i++) {
+                                getAllImageLink(array.getJSONObject(i).getString(FACEBOOK_ID), lengthAlbum);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                GraphRequest req = GraphRequest.newGraphPathRequest(mToken, FACEBOOK_ALBUM, getAlbumIdCallback);
+                Bundle parameters = new Bundle();
+                parameters.putString(FACEBOOK_FIELD, FACEBOOK_ID);
+                req.setParameters(parameters);
+                req.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+    }
+
+    private void getAllImageLink(String albumId, final int lengthAlbum) {
+        GraphRequest.Callback getImageCallback = new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(GraphResponse response) {
+                JSONObject jsonObject = response.getJSONObject();
+                try {
+                    JSONArray array = jsonObject.getJSONArray(FACEBOOK_DATA);
+                    int lengthImage = array.length();
+                    for (int i = 0; i < lengthImage; i++) {
+                        mImageLink.add(array.getJSONObject(i).getString(FACEBOOK_SOURCE));
+                    }
+                    mAlbumGot++;
+                    if (mAlbumGot == lengthAlbum) {
+                        startActivity(new Intent(HomeActivity.this, HomeActivity.class));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        String api = "/" + albumId + FACEBOOK_PHOTO_API;
+        GraphRequest req = GraphRequest.newGraphPathRequest(mToken, api, getImageCallback);
+        Bundle parameters = new Bundle();
+        parameters.putString(FACEBOOK_FIELD, FACEBOOK_SOURCE);
+        req.setParameters(parameters);
+        req.executeAsync();
+
+    }
+
     private void showDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
         builder.setItems(R.array.item_choose_images, new DialogInterface.OnClickListener() {
@@ -68,6 +162,8 @@ public class HomeActivity extends AppCompatActivity {
                         checkPermissionAndPickImage();
                         break;
                     case TYPE_FACEBOOK:
+                        mImageLink.clear();
+                        getImageFromFacebook();
                         break;
                     case TYPE_CAMERA:
                         checkPermissionAndTakePhoto();
@@ -78,6 +174,7 @@ public class HomeActivity extends AppCompatActivity {
         builder.setCancelable(true);
         builder.show();
     }
+
     private void checkPermissionAndPickImage() {
         if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission
                 .READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
@@ -122,6 +219,8 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
             try {
                 checkWriteExternalPermissionAndGetTakenPhoto();
@@ -163,5 +262,9 @@ public class HomeActivity extends AppCompatActivity {
         Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         i.putExtra(MediaStore.EXTRA_OUTPUT, mCaptureImageUri);
         startActivityForResult(i, CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    private void getImageFromFacebook() {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList(FACEBOOK_PHOTO_PERMISSION));
     }
 }
