5a6d071dd884377010a153781a381f566b5ae7d7
framgia.nguyen.van.quang <framgia@framgia0147-lt.framgia.com>
2016-06-16 14:08:20 +0700

    play gif preview

--- app/src/main/java/com/example/hacks_000/giffcreator_02/ui/activity/GifPreviewActivity.java
+++ app/src/main/java/com/example/hacks_000/giffcreator_02/ui/activity/GifPreviewActivity.java
@@ -1,13 +1,12 @@
 package com.example.hacks_000.giffcreator_02.ui.activity;
 
-import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.hacks_000.giffcreator_02.R;
 import com.example.hacks_000.giffcreator_02.data.model.Constant;
@@ -18,12 +17,16 @@ import com.squareup.picasso.Picasso;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GifPreviewActivity extends AppCompatActivity implements MyOnClickListener {
+    private static final int TIME_DELAY = 1000;
     private RecyclerView mRecyclerview;
     private ListImageAdapter mListImageAdapter;
     private List mListImages;
     private ImageView mImagePreviewGif;
+    private int mIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class GifPreviewActivity extends AppCompatActivity implements MyOnClickLi
     }
 
     private void init() {
+        mIndex = 0;
         mListImages = new ArrayList();
         mListImages.add(getIntent().getStringExtra(Constant.INTENT_DATA));
     }
@@ -48,12 +52,42 @@ public class GifPreviewActivity extends AppCompatActivity implements MyOnClickLi
         mRecyclerview.setAdapter(mListImageAdapter);
         mListImageAdapter.setOnItemClickListener(this);
         mImagePreviewGif = (ImageView) findViewById(R.id.gif_preview);
-        Picasso.with(this).load(new File((String)mListImages.get(0)))
+        Picasso.with(this).load(new File((String) mListImages.get(mIndex)))
                 .into(mImagePreviewGif);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new PlayGifAsynTask().execute();
+            }
+        }, TIME_DELAY);
     }
 
     @Override
     public void onItemClick(View view, int position) {
 
+    }
+
+    private class PlayGifAsynTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mIndex++;
+            if (mIndex == mListImages.size()) {
+                mIndex = 0;
+            }
+            Picasso.with(GifPreviewActivity.this)
+                    .load(new File((String) mListImages.get(mIndex)))
+                    .into(mImagePreviewGif);
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    new PlayGifAsynTask().execute();
+                }
+            }, TIME_DELAY);
+        }
     }
 }
